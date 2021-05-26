25a4ef5ebd1a67f6c3beb86c38a4ed69b0e3e1ba
vkazadaev <vkazadaev@buka.ru>
2021-05-26 19:33:18 +0300

    add models for request params and response

---
+++ models/CurrencyRequestModel.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\models;
+
+use yii\base\Model;
+
+class CurrencyRequestModel extends Model
+{
+    public string $currency; // Обязательный параметр, код валюты
+    public string $rateCurrency = 'RUB'; // Необязательный параметр, код валюты, в которой выведется курс, default RUB (например 1 USD = 70 RUB)
+    public int $rateSum = 1; // Необязательный параметр, Сумма первой валюты, default 1. Если sum = 2, то результат будет 10 USD = 700 RUB;
+
+    public function rules()
+    {
+        return [
+            [['currency'], 'required'],
+            ['currency', 'string', 'min' => 3, 'max' => 3],
+            ['rateCurrency', 'string', 'min' => 3, 'max' => 3],
+        ];
+    }
+
+}
