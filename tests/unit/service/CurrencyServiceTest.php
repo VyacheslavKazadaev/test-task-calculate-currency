dc13d56040840d17d2254064c6c31d2b00800c27
vkazadaev <vkazadaev@buka.ru>
2021-05-26 17:40:53 +0300

    add service CurrencyService and test CurrencyServiceTest

---
+++ tests/unit/service/CurrencyServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace tests\unit\service;
+
+class CurrencyServiceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function test()
+    {
+
+    }
+}
