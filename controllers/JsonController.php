7db51507dc675a812e90e93faeca83d7b4e4c517
vkazadaev <vkazadaev@buka.ru>
2021-05-26 17:21:36 +0300

    add controller JsonController.php

---
+++ controllers/JsonController.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\controllers;
+
+use Exception;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\rest\Controller;
+use yii\web\Response;
+
+class JsonController extends Controller
+{
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function afterAction($action, $result)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $this->setResponseErrorIfAny($result);
+
+        return parent::afterAction($action, $result);
+    }
+
+    protected function setResponseError($code)
+    {
+        Yii::$app->response->statusCode = $code;
+    }
+
+    /**
+     * @param $response
+     * @throws Exception
+     */
+    protected function setResponseErrorIfAny($response)
+    {
+        if (is_array($response) && ArrayHelper::keyExists('errors', $response)) {
+            $this->setResponseError(ArrayHelper::getValue($response, 'code', 400));
+        }
+    }
+}
