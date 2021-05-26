49e09b55d0be1c08910e3c16dd1837b648faabb0
vkazadaev <vkazadaev@buka.ru>
2021-05-26 19:33:45 +0300

    inject CurrencyService.php

--- controllers/CurrencyController.php
+++ controllers/CurrencyController.php
@@ -1,10 +1,22 @@
 <?php
 namespace app\controllers;
 
+use app\models\CurrencyRequestModel;
+use app\service\CurrencyService;
+use Yii;
 use yii\filters\VerbFilter;
 
 class CurrencyController extends JsonController
 {
+    private CurrencyService $currencyService;
+
+    public function __construct($id, $module, CurrencyService $currencyService, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+        $this->currencyService = $currencyService;
+    }
+
     public function behaviors()
     {
         return array_merge(parent::behaviors(), [
@@ -19,5 +31,7 @@ class CurrencyController extends JsonController
 
     public function actionIndex()
     {
+        $requestModel = new CurrencyRequestModel(Yii::$app->request->queryParams);
+
     }
 }
