b860cddc1fd992508c62f4f872db84333aab2c85
vkazadaev <vkazadaev@buka.ru>
2021-05-26 19:35:36 +0300

    testing CurrencyService

--- service/CurrencyService.php
+++ service/CurrencyService.php
@@ -1,7 +1,22 @@
 <?php
 namespace app\service;
 
-class CurrencyService
-{
+use app\models\CurrencyRequestModel;
 
+class CurrencyService extends Service
+{
+    private CurrencyGetterClient $client;
+
+    public function __construct(CurrencyGetterClient $client, $config = [])
+    {
+        parent::__construct($config);
+        $this->client = $client;
+    }
+
+    public function loadAndCalculateCurrency(CurrencyRequestModel $currencyRequestModel)
+    {
+        $resultCurrencies = $this->client->requestForGetCurrency();
+
+
+    }
 }
--- tests/unit/service/CurrencyServiceTest.php
+++ tests/unit/service/CurrencyServiceTest.php
@@ -1,24 +1,27 @@
 <?php
 namespace tests\unit\service;
 
+use app\models\CurrencyRequestModel;
+use app\service\CurrencyService;
+use Codeception\Specify;
+
 class CurrencyServiceTest extends \Codeception\Test\Unit
 {
+    use Specify;
+
     /**
      * @var \UnitTester
      */
     protected $tester;
-    
-    protected function _before()
-    {
-    }
 
-    protected function _after()
+    public function testCalculate()
     {
-    }
-
-    // tests
-    public function test()
-    {
+        $service = \Yii::$container->get(CurrencyService::class);
+        $result = $service->loadAndCalculateCurrency(new CurrencyRequestModel([
+            'currency' => 'USD',
+		    'rateCurrency' => 'RUB',
+		    'rateSum' => 1,
+        ]));
 
     }
 }
